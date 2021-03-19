fec4f5300a57e2eb09811a017145fa525853aaa3
SymmetricChaos <ajfraebel@gmail.com>
2021-03-19 07:37:03 -0400

    Update construction.rs

--- examples/construction.rs
+++ examples/construction.rs
@@ -4,10 +4,12 @@ fn main() {
 
     let decision_formula = [
         TNT::new("Ea:(a*SS0)=b"),
+        TNT::new("Aa:~Ec:(SSa*SSc)=b"),
         TNT::new("Aa:[Ec:(a*c)=b>Ed:(d*SS0)=a]"),];
 
     let decision_name = [
-        "b is divisible by two",
+        "b is even",
+        "b is prime",
         "b is a power of two",];
 
     println!("Some valid statements of tnt:");
