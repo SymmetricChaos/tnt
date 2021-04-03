5bc74c121319a94a4eaf5d4ad2f07bb809265d5d
SymmetricChaos <ajfraebel@gmail.com>
2021-04-03 09:08:48 -0400

    BNF for reference

--- src/lib.rs
+++ src/lib.rs
@@ -7,3 +7,15 @@ mod translate;
 pub mod axioms;
 pub mod logic_errors;
 pub mod parser;
+
+/*
+Bakus-Naur Form for the TNT Language
+
+<num> ::= { "0" | "S" <num> }
+<var> ::= { <lowercase_letter> | <var> "'" }
+<arith_op> ::= { "+" | "*" }
+<expr> ::= { num | var | "(" <expr> <arith_op> <expr> ")" | "S" expr }
+<quant> ::= { "A" <var> ":" | "E" <var> ":" | "~" <quant> }
+<logical_op> ::= { "&" | "|" | ">" }
+<formula> ::= {  <expr> "=" <expr> |  "[" <formula> <logical_op> <formula> "]" | <quant> <formula> }
+*/
