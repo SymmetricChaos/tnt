f644f294933de20ad68449d97457efceddf12b71
SymmetricChaos <ajfraebel@gmail.com>
2021-04-15 12:08:29 -0400

    usable primer

--- examples/intro.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-
-    println!("The tnt crate allows simple formal proofs in number theory using Douglas Hofstader's \"Typographic Number Theory\" as a basis. As these are formla proofs not mathematical arguments this introduction will treat all symbols are mere symbols even though they are chosen to be suggestive of familiar mathematical objects. It is necessary to prove that, for instance, the `+` symbol really does correspond to addition of natural numbers.");
-
-    println!("There are three further parts to this introduction. They cover Terms (arthmetic statements), Formulas (logical statements), and Deductions (proofs).");
-
-    println!("The intro continues in: cargo run --example intro_terms")
-
-}
