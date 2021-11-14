0ed7bbacf02e0f9d1c113aa3f54bb97dc179a077
SymmetricChaos <ajfraebel@gmail.com>
2021-11-13 19:07:38 -0500

    display for ast

--- src/parsing/parser.rs
+++ src/parsing/parser.rs
@@ -12,25 +12,33 @@ pub fn build_ast(pair: Pair<Rule>) -> TntNode {
         // Expressions and formulas just mask more specific rules that we want to use
         Rule::formula => build_ast(pair.into_inner().next().unwrap()),
         Rule::expr => build_ast(pair.into_inner().next().unwrap()),
-        Rule::equality => todo!(),
-        Rule::logical_formula => todo!(),
-        Rule::logical_op => todo!(),
-        Rule::quantification => todo!(),
-        Rule::quant => todo!(),
 
+        // Numbers are variables are terminal symbols that we immediately capture
         Rule::num => TntNode::Number(pair.as_str().to_string()),
         Rule::var => TntNode::Variable(pair.as_str().to_string()),
+
+        Rule::equality => {
+            let mut t = pair.into_inner();
+            let lhs = t.next().unwrap();
+            let rhs = t.next().unwrap();
+            TntNode::Equality(Box::new(build_ast(lhs)),Box::new(build_ast(rhs)))
+        },
+        Rule::logical_formula => todo!(),
+        Rule::quantification => todo!(),
+
         Rule::arithmetic_expr => todo!(),
-        Rule::arith_op => todo!(),
-        Rule::successor_expr => todo!(),
+        Rule::successor_expr => TntNode::Successor( Box::new( build_ast(pair.into_inner().next().unwrap())) ),
+        _ => unreachable!()
     }
 }
 
 #[test]
 fn simple_test() {
-    let tnt = "0=a'";
-    let tree = TntParser::parse(Rule::formula, tnt)
+    let tnt = "S0=a''";
+    let tree = TntParser::parse(Rule::equality, tnt)
     .expect("unsuccessful parse") // unwrap the parse result, might fail
     .next().unwrap(); // get and unwrap the outermost rule, never fails
-    println!("{:#?}",tree)
+    println!("{:#?}",tree);
+    let ast = build_ast(tree);
+    println!("{}",ast);
 }
--- src/parsing/syntax_tree.rs
+++ src/parsing/syntax_tree.rs
@@ -1,20 +1,53 @@
+use std::fmt::Display;
 
+#[derive(Debug)]
 pub enum Quant {
     Existential(String),
     Universal(String)
 }
 
+impl Display for Quant {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Quant::Existential(v) => write!(f, "E{}:", v),
+            Quant::Universal(v) => write!(f, "A{}:", v),
+        }
+    }
+}
+
+#[derive(Debug)]
 pub enum LogicOp {
     And,
     Or,
     Implies
 }
 
+impl Display for LogicOp {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            LogicOp::And => write!(f, "&"),
+            LogicOp::Or => write!(f, "|"),
+            LogicOp::Implies => write!(f, ">"),
+        }
+    }
+}
+
+#[derive(Debug)]
 pub enum ArithmeticOp {
     Add,
     Mul
 }
 
+impl Display for ArithmeticOp {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ArithmeticOp::Add => write!(f, "+"),
+            ArithmeticOp::Mul => write!(f, "*"),
+        }
+    }
+}
+
+#[derive(Debug)]
 pub enum TntNode {
     Equality(Box<TntNode>, Box<TntNode>),
     Arithmetic(ArithmeticOp, Box<TntNode>, Box<TntNode>),
@@ -23,4 +56,18 @@ pub enum TntNode {
     Quantification(Quant, Box<TntNode>),
     Number(String),
     Variable(String),
+}
+
+impl Display for TntNode {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            TntNode::Equality(lhs, rhs) => write!(f, "{}={}", lhs, rhs),
+            TntNode::Arithmetic(op, lhs, rhs) => write!(f, "{}{}{}", lhs, op, rhs),
+            TntNode::Logical(op, lhs, rhs) => write!(f, "{}{}{}", lhs, op, rhs),
+            TntNode::Successor(contents) => write!(f, "S{}", contents),
+            TntNode::Quantification(q, contents) => write!(f, "{}{}", q, contents),
+            TntNode::Number(n) => write!(f, "{}", n),
+            TntNode::Variable(v) => write!(f, "{}", v),
+        }
+    }
 }
