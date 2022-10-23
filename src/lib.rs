712e527c5cd4057ee286ec65fbc2230d6000681f
SymmetricChaos <ajfraebel@gmail.com>
2022-10-23 08:58:29 -0400

    remove old files

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-//pub mod deduction;
+pub mod deduction;
+pub use deduction::Deduction;
 pub mod formula;
 pub use formula::{and, eq, exists, forall, implies, not, or, Formula, PEANO};
 pub mod term;
@@ -6,6 +7,7 @@ pub use term::{prod, succ, sum, Term, ONE, ZERO};
 //mod properties;
 //pub mod axioms;
 pub mod logic_errors;
+pub use logic_errors::LogicError;
 pub mod production;
 //mod translate;
 
