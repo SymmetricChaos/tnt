7786f0a45002f4929bef13d4d0674a7c56134071
SymmetricChaos <ajfraebel@gmail.com>
2022-11-02 18:56:32 -0400

    removed unused imports

--- src/lib.rs
+++ src/lib.rs
@@ -4,14 +4,10 @@ pub mod formula;
 pub use formula::{and, eq, exists, forall, implies, not, or, Formula, PEANO};
 pub mod term;
 pub use term::{prod, succ, sum, Term, ONE, ZERO};
-//mod properties;
-//pub mod axioms;
 pub mod logic_errors;
 pub use logic_errors::LogicError;
 pub mod production;
 pub use production::*;
-//mod translate;
-
 pub mod parsing;
 
 #[macro_use]
